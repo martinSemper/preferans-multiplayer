96831f7332d3bbb03c8239b94f668b02c622c86a
martin.semper <martin.semper@corvus.hr>
2016-05-04 15:39:21 +0200

    adding a host

---
+++ Preferans/SignalRHost/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalRHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "http://localhost:8080";
+
+            using(Microsoft.Owin.Hosting.WebApp.Start(url))
+            {
+                Console.WriteLine("Sever running on {0}", url);
+                Console.ReadLine();
+            }
+        }
+    }
+}
