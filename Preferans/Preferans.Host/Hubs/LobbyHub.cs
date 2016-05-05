031201437b452380d2fa37dd73855c0e7b626ec2
martin.semper <martin.semper@corvus.hr>
2016-05-05 15:20:09 +0200

    adding a host

---
+++ Preferans/Preferans.Host/Hubs/LobbyHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Preferans.Host.Hubs
+{
+    class LobbyHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            this.Clients.All.addMessage(name, message);
+        }   
+    }
+}
---
+++ Preferans/Preferans.Host/Startup.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Cors;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(Preferans.Host.Startup))]
+namespace Preferans.Host
+{
+    class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCors(CorsOptions.AllowAll);
+            app.MapSignalR();
+        }
+    }
+}
