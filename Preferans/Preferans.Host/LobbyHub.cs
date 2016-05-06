895b57642755fea51231374c3c0e9ed5d5d333e2
martin.semper <martin.semper@corvus.hr>
2016-05-06 16:15:36 +0200

    daily progress

---
+++ Preferans/Preferans.Host/LobbyHub.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Preferans.Host
+{
+    public class LobbyHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            this.Clients.All.addMessage(name, message);
+        }
+
+        public void MakeMove(string name)
+        {
+            this.Clients.All.makeMove(name);
+        }
+    }
+}
---
+++ Preferans/Preferans.Host/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Preferans.Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "http://localhost:8080";
+
+            using (WebApp.Start(url))
+            {
+                Console.WriteLine("Sever running on {0}", url);
+                Console.ReadLine();
+            }            
+        }
+    }
+}
